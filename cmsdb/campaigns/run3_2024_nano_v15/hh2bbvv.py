d3e0ffafedfc51c24b7690b3be2e2db7470664d2
Robert James Ward <robert.james.ward@cern.ch>
2026-07-21 13:21:22 +0200

    Uncomment includes

--- cmsdb/campaigns/run3_2024_nano_v15/hh2bbvv.py
+++ cmsdb/campaigns/run3_2024_nano_v15/hh2bbvv.py
@@ -4,8 +4,8 @@
 HH -> bbVV datasets for the 2024 data-taking campaign with datasets at NanoAOD tier in version 15.
 """
 
-# import cmsdb.processes as procs
-# from cmsdb.campaigns.run3_2024_nano_v15 import campaign_run3_2024_nano_v15 as cpn
+import cmsdb.processes as procs
+from cmsdb.campaigns.run3_2024_nano_v15 import campaign_run3_2024_nano_v15 as cpn
 
 
 #
